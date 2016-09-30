8703f5bfb7e9997082e0a7e65a997e46810e9c37
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-09-30 15:19:11 +0300

    Create menu_items_actions.sql

---
+++ menuForSymfony/menu_items_actions.sql
@@ -0,0 +1,37 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Структура таблицы `menu_items_actions`
+--
+
+CREATE TABLE IF NOT EXISTS `menu_items_actions` (
+  `menuitems_id` int(11) NOT NULL,
+  `action_id` int(11) NOT NULL,
+  PRIMARY KEY (`menuitems_id`,`action_id`),
+  KEY `IDX_DF0BFC7897235247` (`menuitems_id`),
+  KEY `IDX_DF0BFC789D32F035` (`action_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `menu_items_actions`
+--
+
+INSERT INTO `menu_items_actions` (`menuitems_id`, `action_id`) VALUES
+(4, 11),
+(5, 9),
+(6, 9),
+(7, 9),
+(8, 14),
+(10, 14),
+(18, 13);
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `menu_items_actions`
+--
+ALTER TABLE `menu_items_actions`
+  ADD CONSTRAINT `FK_DF0BFC7897235247` FOREIGN KEY (`menuitems_id`) REFERENCES `menu_items` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DF0BFC789D32F035` FOREIGN KEY (`action_id`) REFERENCES `auth_action` (`id`);
