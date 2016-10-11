175374ab51bd1d3f407ea8ba99aac712b8765ddb
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-10-11 11:23:19 +0300

    Update menu.sql

--- menuForSymfony/menu.sql
+++ menuForSymfony/menu.sql
@@ -1,4 +1,10 @@
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+--
+-- Структура таблицы `menu`
+--
 
 CREATE TABLE IF NOT EXISTS `menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -9,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_7D053A935E237E06` (`name`),
   UNIQUE KEY `UNIQ_7D053A93E16C6B94` (`alias`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
 
 --
 -- Дамп данных таблицы `menu`
@@ -17,4 +23,4 @@ CREATE TABLE IF NOT EXISTS `menu` (
 
 INSERT INTO `menu` (`id`, `name`, `published`, `alias`, `class`) VALUES
 (1, 'CRM Admin', 1, 'crm_admin', 'crm-menu'),
-(2, 'ClientSidebar', 1, 'client_sidebar', 'not');
+(2, 'ClientSidebar', 1, 'client_sidebar', 'not'),
