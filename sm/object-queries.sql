68911c562f115c4b234a0321dc7c30f3449ebae8
Robert Kuznetsov <robot3@xaker.ru>
2017-04-12 22:38:47 +0300

    Add query for SM.

--- sm/object-queries.sql
+++ sm/object-queries.sql
@@ -1,3 +1,8 @@
-SELECT MIN(  `god_postrojki` ) 
-FROM  `object` 
+SELECT MIN(  `god_postrojki` )
+FROM  `object`
 WHERE  `objekt` = 3 OR `objekt` = 4
+
+--
+select `ploshad_o`, `ploshad_u`, `ploshad_k`, `ploshad_zh`, `komnat_vsego`, `kmotminska`
+FROM `object`
+WHERE `objekt` = 3 AND `ploshad_o` >= 0 AND `ploshad_k` >= 0 AND `ploshad_zh` >= 0 AND `komnat_vsego` >= 0 AND`kmotminska`>=0;
